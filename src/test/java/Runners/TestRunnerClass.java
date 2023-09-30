4313e82404323fa86835ac3c0f654bd9f427a193
Enes <enes.buke12@gmail.com>
2023-09-30 12:09:10 +0300

    Initial commit

---
+++ src/test/java/Runners/TestRunnerClass.java
@@ -0,0 +1,10 @@
+package Runners;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions
+
+public class TestRunnerClass extends AbstractTestNGCucumberTests {
+}
