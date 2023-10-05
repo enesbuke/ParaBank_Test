c2543d6ca6ef941d7df9cc6413626bec62bd1149
Enes <enes.buke12@gmail.com>
2023-10-05 18:09:17 +0300

    Update to project #8 to Enes

--- src/test/java/Runners/TestRunnerClass.java
+++ src/test/java/Runners/TestRunnerClass.java
@@ -1,10 +1,28 @@
 package Runners;
 
 
+import com.aventstack.extentreports.service.ExtentService;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 
-@CucumberOptions
+@CucumberOptions(
+        features = {
+                "src/test/java/FeatureFiles"},
+        glue={"StepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
 
 public class TestRunnerClass extends AbstractTestNGCucumberTests {
+
+    @AfterClass
+    public static void writeExtentReport() {
+        ExtentService.getInstance().setSystemInfo("Windows User Name", System.getProperty("user.name"));
+        ExtentService.getInstance().setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+        ExtentService.getInstance().setSystemInfo("User Name", "Enes BÜKE");
+        ExtentService.getInstance().setSystemInfo("Application Name", "Campus");
+        ExtentService.getInstance().setSystemInfo("Operating System Info", System.getProperty("os.name"));
+        ExtentService.getInstance().setSystemInfo("Department", "QA");
+
+    }
 }
